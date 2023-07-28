ed5aa503ece9c2c29326c93ee4ef1c387d19c1b7
Kyle Hassett <22921249+kylehassett@users.noreply.github.com>
2023-07-28 16:04:10 -0400

    added python related ignore to telescope patterns

--- neovim/lua/init-plugins-config.lua
+++ neovim/lua/init-plugins-config.lua
@@ -24,12 +24,16 @@ vim.env.FZF_DEFAULT_COMMAND = 'rg --files -g "!.git/" -g "!.github/" -g "!node_m
 -- Indent blankline
 vim.opt.listchars:append "eol:↴"
 
-vim.cmd [[highlight IndentBlanklineIndent1 guibg=#184e77 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1e6091 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guibg=#1a759f gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guibg=#168aad gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guibg=#34a0a4 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guibg=#52b69a gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guibg=#36384a gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guibg=#404459 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guibg=#4b4f68 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guibg=#565a76 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guibg=#606585 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guibg=#6b7194 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent7 guibg=#7a7f9f gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent8 guibg=#898da9 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent9 guibg=#979bb4 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent10 guibg=#a6aabf gui=nocombine]]
 
 require("indent_blankline").setup {
     char = "",
@@ -40,6 +44,10 @@ require("indent_blankline").setup {
         "IndentBlanklineIndent4",
         "IndentBlanklineIndent5",
         "IndentBlanklineIndent6",
+        "IndentBlanklineIndent7",
+        "IndentBlanklineIndent8",
+        "IndentBlanklineIndent9",
+        "IndentBlanklineIndent10",
     },
     space_char_highlight_list = {
         "IndentBlanklineIndent1",
@@ -48,6 +56,10 @@ require("indent_blankline").setup {
         "IndentBlanklineIndent4",
         "IndentBlanklineIndent5",
         "IndentBlanklineIndent6",
+        "IndentBlanklineIndent7",
+        "IndentBlanklineIndent8",
+        "IndentBlanklineIndent9",
+        "IndentBlanklineIndent10",
     },
     show_current_context = true,
     show_current_context_start = true,
@@ -84,7 +96,11 @@ local grep_ignore_globs = {
     "--glob",
     "!**/.next/*",
     "--glob",
-    "!**/build/*"
+    "!**/build/*",
+    "--glob",
+    "!**/__pycache__/*",
+    "--glob",
+    "!**/.pytest_cache/*"
 }
 
 telescope.load_extension("media_files")
