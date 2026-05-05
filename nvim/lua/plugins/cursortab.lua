48af1eabdee07e03855582dd4171a79a2b221013
Kyle Hassett <22921249+kylehassett@users.noreply.github.com>
2026-05-04 23:24:53 -0400

    Got cursortab to work locally with open source sweep model

--- nvim/lua/plugins/cursortab.lua
+++ nvim/lua/plugins/cursortab.lua
@@ -5,14 +5,8 @@ return {
         build = 'cd server && go build',
         config = function()
             require('cursortab').setup({
-                ui = {
-                    completions = {
-                        addition_style = "highlight",
-
-                    }
-                },
                 provider = {
-                    type = "zeta-2",
+                    type = "sweep",
                     url = "http://192.168.1.9:1234"
                 }
             })
